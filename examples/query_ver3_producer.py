9a930dd71cdec6e0be909555695e1a154db7d6db
Cornelius Toole <ctoole@zenoss.com>
2017-10-23 20:19:47 -0500

    add metric timestamp start/end support

--- examples/query_ver3_producer.py
+++ examples/query_ver3_producer.py
@@ -17,7 +17,11 @@ else:
 
 value_schema = avro.load('query2.avsc')
 key_schema = avro.load('query_id.avsc')
-value = {"id": str(uuid4()), "query": query_string, "metadata": {"timestamp": str(int(time.time())),"tenant_id": "ACME_CORP"}, "querytype": "TYPED", "queryParams": {"query": query_string, "tenant": "ACME_CORP", "metricstart": "1234567", "metricend": "1234567", "sortby": "CPU", "sortorder": "ASC"}}
+now_in_secs = time.time()
+one_hr_ago_in_secs = now_in_secs - 60*60;
+start_ts = time.strftime("%Y-%m-%dT%H:%M:%S.000Z", time.gmtime(one_hr_ago_in_secs))
+end_ts = time.strftime("%Y-%m-%dT%H:%M:%S.000Z", time.gmtime(now_in_secs))
+value = {"id": str(uuid4()), "query": query_string, "metadata": {"timestamp": str(int(time.time())),"tenant_id": "ACME_CORP"}, "querytype": "TYPED", "queryParams": {"query": query_string, "tenant": "ACME_CORP", "start": "2017-10-23T00:00:00.000Z", "end": end_ts, "sortby": "CPU", "sortorder": "ASC"}}
 
 key = "query_producer3"
 
