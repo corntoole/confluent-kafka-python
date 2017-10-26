06bc5d49874f9c4a46d709a7c9c6b835f1125b7b
Cornelius Toole <ctoole@zenoss.com>
2017-10-26 16:18:27 -0500

    updated script to interoperate with latest ZING domain query layer apps

--- examples/query_ver3_producer.py
+++ examples/query_ver3_producer.py
@@ -7,7 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Publishes Domain Query messages to Kafka')
 parser.add_argument('--query', type=str)
-
+parser.add_argument('--tenant', type=str)
 args = parser.parse_args()
 
 if args.query is None:
@@ -15,13 +15,19 @@ if args.query is None:
 else:
     query_string = args.query
 
+
+if args.tenant is None:
+    tenant = 'anyone'
+else:
+    tenant = args.tenant
+
 value_schema = avro.load('query2.avsc')
 key_schema = avro.load('query_id.avsc')
 now_in_secs = time.time()
 one_hr_ago_in_secs = now_in_secs - 60*60;
 start_ts = time.strftime("%Y-%m-%dT%H:%M:%S.000Z", time.gmtime(one_hr_ago_in_secs))
 end_ts = time.strftime("%Y-%m-%dT%H:%M:%S.000Z", time.gmtime(now_in_secs))
-value = {"id": str(uuid4()), "query": query_string, "metadata": {"timestamp": str(int(time.time())),"tenant_id": "ACME_CORP"}, "querytype": "TYPED", "queryParams": {"query": query_string, "tenant": "ACME_CORP", "start": "2017-10-23T00:00:00.000Z", "end": end_ts, "sortby": "CPU", "sortorder": "ASC"}}
+value = {"id": str(uuid4()), "query": query_string, "metadata": {"timestamp": str(int(time.time())),"tenant_id": tenant}, "querytype": "TYPED", "queryParams": {"query": query_string, "tenant": tenant, "start": "2017-10-23T00:00:00.000Z", "end": end_ts, "sortby": "CPU", "sortorder": "ASC"}}
 
 key = "query_producer3"
 
